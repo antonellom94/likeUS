648c7dbe38e11ad6a94fb3acc814c11bf86c42a2
Antonello <antonellomele94@gmail.com>
2021-03-29 12:44:37 +0200

    update gitignore and deleted sec folders

--- server/config/keys.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  twitterAPIKey: "a2xyZrVIUxe5vACdVKDy0nTVI",
-  twitterAPISecretKey: "tOaeCkTkqgPIU5U7L09IdAKztBrqj60RXrirUAdbkZXUhoMG0h",
-  twitterAPIToken:
-    "AAAAAAAAAAAAAAAAAAAAAIBnOAEAAAAAWEVBIJVyzs%2F43h%2F7NbI55Oz2kI8%3DLDN3FCllZAb20Ll5tGPjSuPzYmIxxC3ymU2IWnlVbj1crlSpEj",
-};
